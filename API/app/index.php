8fedba56faa90aff9106b1c7a35654808d3de4b8
Joshua Okello <Joshua Okello>
2016-11-15 16:43:57 +0300

    first attempt at location service for safepal

--- API/app/index.php
+++ API/app/index.php
@@ -2,8 +2,12 @@
 
 session_start();
 
+//PSR
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
+
+//geocoder
+use \Geocoder\Provider\GoogleMaps as gmaps;
 
 require '../vendor/autoload.php';
 
@@ -70,6 +74,8 @@ $app->post('/api/v1/reports/new', function (Request $req, Response $res){
     		//return status and data
     		return $res->withJson(getenv($FAILURE));
     	}
+
+    	$this->pdo = null; //close connection
 	}
 });
 
@@ -86,6 +92,48 @@ $app->get('/api/v1/reports/{rid}', function (Request $req, Response $res){
 //delete report
 $app->post('/api/v1/reports/del/{rid}', function (Request $req, Response $res){
 	$rID = $req->getAttribute('rid');
+});
+
+//get nearest health centres based on GPS
+$app->post('/api/v1/locations/nearest', function (Request $req, Response $res){
+	$gps = $req->getParsedBody();
+
+	//quick check
+	if (!empty($gps)) {
+		//curl http adapter -- *note: should be optional since slim already implements PSR-7
+		$curl = new \Ivory\HttpAdapter\CurlHttpAdapter();
+		$gmaps = new gmaps($curl);
+
+		//get district name
+		$district = $gmaps->reverse($gps['reporter_lat'], $gps['reporter_long'])->getLocality();
+
+		$centres = $this->pdo->query("SELECT * FROM cso_details WHERE cso_location = ".$district);
+
+		$nearest = new array();
+
+		//**note: consider refactoring here
+		if (sizeof($centres) > 0) {
+
+			for ($centres as $key => $value) {
+				//calculate distance -- could be optional since we are sending back centres in the same district
+
+				$theta = $gps['reporter_long'] - $centres['cso_longtitude'];
+				$d = sin(deg2rad($gps['reporter_lat'])) * sin(deg2rad($centres['cso_latitude'])) 
+					+ cos(deg2rad($gps['reporter_lat'])) * cos(deg2rad($centres['cso_latitude'])) * cos(deg2rad($theta));
+				$d = (float) rad2deg(acos($d)) * 69.09; //converted to miles from nautical miles? (60 * 1.1515)
+
+				//construct result array
+				array_push($nearest, array("cso_name" => $centres["cso_name"], "cso_location" => $centres["cso_location"], 
+					"cso_distance" => $d * (int) 1.61, "cso_phone_number" => $centres["cso_phone_number"]));
+			}
+		}
+
+		$this->pdo = null; //close connection
+
+		//return json
+		$req->withJson(sort($nearest));
+	}
+
 });
 
 
