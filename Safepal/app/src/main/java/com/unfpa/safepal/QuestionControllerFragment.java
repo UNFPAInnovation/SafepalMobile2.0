e6b31083877942ae3871a570f2f6498c36a01027
codephillip <codephillip@gmail.com>
2020-04-17 13:26:13 +0300

    added quiz progress bar params and change logic

--- Safepal/app/src/main/java/com/unfpa/safepal/QuestionControllerFragment.java
+++ Safepal/app/src/main/java/com/unfpa/safepal/QuestionControllerFragment.java
@@ -1,5 +1,6 @@
 package com.unfpa.safepal;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,15 +9,21 @@ import android.view.ViewGroup;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.navigation.fragment.NavHostFragment;
+
+import com.mikhaellopez.circularprogressbar.CircularProgressBar;
 
 import timber.log.Timber;
 
 public class QuestionControllerFragment extends Fragment implements View.OnClickListener {
 
     Button yesButton, noButton, sometimesButton;
+    TextView questionNumber, questionText;
+    float progressCount = 10f;
+    private CircularProgressBar circularProgressBar;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -29,6 +36,18 @@ public class QuestionControllerFragment extends Fragment implements View.OnClick
         yesButton = view.findViewById(R.id.yes_button);
         noButton = view.findViewById(R.id.no_button);
         sometimesButton = view.findViewById(R.id.sometimes_button);
+        questionNumber = view.findViewById(R.id.question_number);
+        questionText = view.findViewById(R.id.question_text);
+
+        circularProgressBar = view.findViewById(R.id.progress_circular);
+        circularProgressBar.setProgress(progressCount);
+        circularProgressBar.setProgressMax(100f);
+        circularProgressBar.setProgressBarColor(getResources().getColor(R.color.colorAccent));
+        circularProgressBar.setBackgroundProgressBarColor(getResources().getColor(R.color.colorGrey));
+        circularProgressBar.setProgressBarWidth(5f);
+        circularProgressBar.setBackgroundProgressBarWidth(5f);
+        circularProgressBar.setStartAngle(45f);
+        circularProgressBar.setProgressDirection(CircularProgressBar.ProgressDirection.TO_LEFT);
 
         yesButton.setOnClickListener(QuestionControllerFragment.this);
         noButton.setOnClickListener(QuestionControllerFragment.this);
@@ -51,6 +70,9 @@ public class QuestionControllerFragment extends Fragment implements View.OnClick
                 Timber.d("clicked nothing");
                 break;
         }
+
+        progressCount += progressCount;
+        circularProgressBar.setProgress(progressCount);
 
         Toast.makeText(view.getContext(), "clicked", Toast.LENGTH_SHORT).show();
 //        NavHostFragment.findNavController(QuestionControllerFragment.this)
