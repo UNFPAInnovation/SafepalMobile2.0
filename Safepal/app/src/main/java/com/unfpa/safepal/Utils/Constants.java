f9bf8c4f5ecea893f8fb40e4960b8f9a5473f83a
codephillip <codephillip@gmail.com>
2020-05-28 20:35:40 +0300

    added localhost emulator configs

--- Safepal/app/src/main/java/com/unfpa/safepal/Utils/Constants.java
+++ Safepal/app/src/main/java/com/unfpa/safepal/Utils/Constants.java
@@ -1,7 +1,8 @@
 package com.unfpa.safepal.Utils;
 
 public class Constants {
-    final public static String BASE_API_URL = "http://ec2-54-184-113-209.us-west-2.compute.amazonaws.com/api/v1";
+    final public static String BASE_API_URL = "http://10.0.2.2";
+//    final public static String BASE_API_URL = "http://ec2-54-184-113-209.us-west-2.compute.amazonaws.com/api/v1";
 //    final public static String BASE_API_URL = "https://api.safepal.co/api/v1";
     final public static int SOCKET_TIMEOUT = 30000;
     final public static String phoneNumberDashRegex = "^(0)7[0-9]{1}-\\d{3}-\\d{4}";
